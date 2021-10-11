e15b43213de5665b3617ff56999ac84b67645fde
jadick <adickja@gmail.com>
2021-10-11 06:45:22 -0400

    styling changes for sign in page

--- client/src/screens/SignIn/SignIn.jsx
+++ client/src/screens/SignIn/SignIn.jsx
@@ -1,5 +1,30 @@
 import { useState } from "react";
-import { Link } from "react-router-dom";
+
+import Paper from "@mui/material/Paper";
+import TextField from "@mui/material/TextField";
+import AppBar from "@mui/material/AppBar";
+import Button from "@mui/material/Button";
+import CssBaseline from "@mui/material/CssBaseline";
+import Grid from "@mui/material/Grid";
+import Stack from "@mui/material/Stack";
+import Box from "@mui/material/Box";
+import Toolbar from "@mui/material/Toolbar";
+import Typography from "@mui/material/Typography";
+import Container from "@mui/material/Container";
+import Link from "@mui/material/Link";
+
+function Copyright() {
+  return (
+    <Typography variant="body2" color="text.secondary" align="center">
+      {"Copyright © "}
+      <Link color="inherit" href="https://material-ui.com/">
+        DevelopingMobility
+      </Link>{" "}
+      {new Date().getFullYear()}
+      {"."}
+    </Typography>
+  );
+}
 
 export default function SignIn(props) {
   const [formData, setFormData] = useState({
@@ -16,37 +41,116 @@ export default function SignIn(props) {
   };
 
   return (
-    <div>
-      <form
-        onSubmit={(e) => {
-          e.preventDefault();
-          props.handleLogin(formData);
-        }}
-      >
-        <h3>Login</h3>
-        <label>
-          Username:
-          <input
-            type="text"
-            name="username"
-            value={formData.username}
-            onChange={handleChange}
+    <>
+      <CssBaseline />
+      <AppBar position="relative" sx={{ height: 100 }}>
+        <Toolbar>
+          <img
+            src="https://res.cloudinary.com/dy6xpqkkj/image/upload/c_fill,g_north,h_95,w_150,x_0,y_0/v1633639529/DevelopingMobility/f00225dbda284b8e8d0300962a571784_3_fdiiaz.png"
+            alt="logo"
           />
-        </label>
-        <br />
-        <label>
-          Password:
-          <input
-            type="password"
-            name="password"
-            value={formData.password}
-            onChange={handleChange}
-          />
-        </label>
-        <br />
-        <Link to="/register">Register</Link>
-        <button>Submit</button>
-      </form>
-    </div>
+          <Typography
+            component="h1"
+            variant="h2"
+            align="center"
+            fontFamily="'Didact Gothic', sans-serif;"
+            letterSpacing="6px"
+          >
+            DevelopingMobility
+          </Typography>
+        </Toolbar>
+      </AppBar>
+      <main>
+        <Box
+          sx={{
+            bgcolor: "#F4EEF2",
+            pt: 8,
+            pb: 6,
+          }}
+        >
+          <Container maxWidth="sm">
+            <form
+              onSubmit={(e) => {
+                e.preventDefault();
+                props.handleLogin(formData);
+              }}
+            >
+              <Stack
+                sx={{ pt: 4 }}
+                direction="column"
+                spacing={2}
+                justifyContent="center"
+              >
+                <TextField
+                  required
+                  id="filled-required"
+                  label="Username"
+                  name="username"
+                  value={formData.username}
+                  variant="filled"
+                  onChange={handleChange}
+                />
+                <TextField
+                  required
+                  type="password"
+                  id="filled-required"
+                  label="Password"
+                  name="password"
+                  value={formData.password}
+                  variant="filled"
+                  onChange={handleChange}
+                />
+              </Stack>
+              <Stack direction="row" spacing={2} justifyContent="center">
+                <Box
+                  component="img"
+                  sx={{
+                    height: 233,
+                    width: 350,
+                    maxHeight: { xs: 500, md: 500 },
+                    maxWidth: { xs: 500, md: 500 },
+                  }}
+                  alt="Stretching at home."
+                  src="https://res.cloudinary.com/dy6xpqkkj/image/upload/v1633944672/DevelopingMobility/Exercising_at_home_xmazyy.png"
+                />
+                <Stack
+                  sx={{ pt: 1 }}
+                  direction="column"
+                  spacing={2}
+                  justifyContent="center"
+                >
+                  <Button type="submit" variant="contained">
+                    Log In
+                  </Button>
+                  <Typography
+                    variant="h5"
+                    align="center"
+                    color="text.secondary"
+                    paragraph
+                  >
+                    Time to stretch
+                  </Typography>
+                </Stack>
+              </Stack>
+            </form>
+          </Container>
+        </Box>
+      </main>
+      {/* Footer */}
+      <Box sx={{ bgcolor: "#F4EEF2", p: 6 }} component="footer">
+        <Typography variant="h6" align="center" gutterBottom>
+          Congratulations Chickadees!
+        </Typography>
+        <Typography
+          variant="subtitle1"
+          align="center"
+          color="text.secondary"
+          component="p"
+        >
+          Thank you for your visit, have a wonderful day
+        </Typography>
+        <Copyright />
+      </Box>
+    </>
   );
 }
